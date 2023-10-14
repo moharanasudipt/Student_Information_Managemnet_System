ca8ede7f1dbbcc6e47e50a2c7ade735218ea336e
SAAYAN-SAMANTA <128898872+SAAYAN-SAMANTA@users.noreply.github.com>
2023-10-14 22:06:40 +0530

    Update Dao.java

--- src/java/Dao/Dao.java
+++ src/java/Dao/Dao.java
@@ -9,6 +9,7 @@ public interface Dao {
     public String checkA(String email, String password);
     public String checkS(String email, String password);
     public boolean addStudent(String name, String address, int age, String dob, int contact, String gender, String gname, String mail, String branch, String password, String photo);
+    public int updateFees(int CurrentFee,int AmountFee,String email);
     public String generatePassword(String ename, String email);
     public boolean addResult(String email,String branch,Double CGPA);
     //for departments
