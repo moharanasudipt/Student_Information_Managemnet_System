409e770749f254cff8de81ad88ae26fc3b215a08
Sudipt Kumar Moharana <104424491+moharanasudipt@users.noreply.github.com>
2023-10-15 22:07:59 +0530

    Update LoginServlet.java

--- src/java/LoginServlet.java
+++ src/java/LoginServlet.java
@@ -20,8 +20,11 @@ public class LoginServlet extends HttpServlet {
         String admin = "Admin";
         String student = "Student";
         String role = null;
-        
-
+        String adName=null;
+        String photo=null;
+        List<Admin> adminList= new ArrayList<Admin>();
+        HttpSession session=request.getSession();
+       
         ServletConfig sc = getServletConfig();
         String HR_id = sc.getInitParameter("HR_ID");
         System.out.println(HR_id);
@@ -36,6 +39,13 @@ public class LoginServlet extends HttpServlet {
         if (!HR_id.equals(email)) {
             role = dao.checkA(email, password);
             System.out.println("RoleA" + role);
+            if(role!=null){
+            adminList=dao.getAdminDetails(email);
+            Admin adminDetails=adminList.get(0);
+            adName=adminDetails.getName();
+            photo = adminDetails.getPhoto();
+            
+            }
             if(role==null){
             role = dao.checkS(email, password);
             System.out.println("RoleS" + role);
@@ -48,6 +58,10 @@ public class LoginServlet extends HttpServlet {
             rd.forward(request, response);
         } else if (admin.equalsIgnoreCase(role)) {          
             System.out.println("else if Admin");
+            List<Student> StudentDetails=dao.getAllStudentDetails();
+            session.setAttribute("AdminName", adName);
+            session.setAttribute("AdminPhoto", photo);      
+            session.setAttribute("StudentDetails", StudentDetails);                  
             RequestDispatcher rd = request.getRequestDispatcher("Admin.jsp");
             rd.forward(request, response);
         } else if (student.equalsIgnoreCase(role)) {
