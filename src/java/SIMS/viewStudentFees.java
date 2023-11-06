bd5bdd7f9b1ffe34b4943407cec1748ef95366db
SUSANTA KUMAR DEBATA <142401059+SusantaDebata@users.noreply.github.com>
2023-11-06 20:09:33 +0530

    viewStudentFees.java Servlet is completed

--- src/java/SIMS/viewStudentFees.java
+++ src/java/SIMS/viewStudentFees.java
@@ -1,1 +1,36 @@
+package SIMS;
+import Dao.*;
+import Model.Student;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.List;
 
+public class viewStudentFees extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        
+        try {            
+            String SEmail = request.getParameter("email");           
+            System.out.println(SEmail);
+            
+            ServletContext context = getServletContext();
+            Dao dao = (Dao) context.getAttribute("db");
+          
+            List<Student> SProfile = dao.getStudent(SEmail);
+            System.out.println(SProfile);
+            if (SProfile!=null) {
+                System.out.println("Result Show Successfully ");
+                request.setAttribute("allStudentProfile", SProfile);                
+                RequestDispatcher rd = request.getRequestDispatcher("viewStudentFees.jsp");
+                rd.forward(request, response);
+            } else {                
+                RequestDispatcher rd = request.getRequestDispatcher("Student.jsp");
+                rd.forward(request, response);
+            }
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
