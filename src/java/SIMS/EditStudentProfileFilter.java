1c87ede6086fab82a1d120807c949e1addf76c8a
Debasis Nishank <95074176+debasisnishank@users.noreply.github.com>
2023-11-15 06:45:18 +0530

    Updated EditStudentProfileFilter.java
    Added methods for handling error

--- src/java/SIMS/EditStudentProfileFilter.java
+++ src/java/SIMS/EditStudentProfileFilter.java
@@ -92,4 +92,60 @@ public class EditStudentProfileFilter implements Filter {
     public void setFilterConfig(FilterConfig filterConfig) {
         this.filterConfig = filterConfig;
     }
+ @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {
+                log("LoginServletFilter:Initializing filter");
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("LoginServletFilter()");
+        }
+        StringBuffer sb = new StringBuffer("LoginServletFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+
+    private void sendProcessingError(Throwable t, ServletResponse response) {
+        String stackTrace = getStackTrace(t);
+
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
+                pw.print(stackTrace);
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    
 }
